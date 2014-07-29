86f7583343fcbb414fe407edfc61369f1cadad90
Austin A Baty <abaty@hidsk0001.cmsaf.mit.edu>
2014-07-29 12:25:12 -0400

    deleted a few unnecessary lines of code

--- run/run_fake.sh
+++ run/run_fake.sh
@@ -6,10 +6,7 @@ fi
 
 echo | awk -v i=$1 '{print "./run_fake.exe "i" "}' 
 echo | awk -v i=$1 '{print "./run_fake.exe "i" "}' | bash
-echo | awk -v tag=$4 -v user=$USER '{print "mkdir -p /net/hisrv0001/home/"user"/scratch_proxy/HIRun2013/merged"}'
-echo | awk -v tag=$4 -v user=$USER '{print "mkdir -p /net/hisrv0001/home/"user"/scratch_proxy/HIRun2013/merged"}' | bash
-echo | awk -v tag=$4 -v user=$USER '{print "mkdir -p /net/hisrv0001/home/"user"/scratch_proxy/HIRun2013/umerged"}'
-echo | awk -v tag=$4 -v user=$USER '{print "mkdir -p /net/hisrv0001/home/"user"/scratch_proxy/HIRun2013/unmerged"}' | bash
+
 echo | awk -v tag=$4 -v user=$USER '{print "mv fake_pt*.root /net/hisrv0001/home/"user"/tracking_eff_corrections/CMSSW_5_3_12_patch3/src/condor_trk_corr/final_hists/"}'
 echo | awk -v tag=$4 -v user=$USER '{print "mv fake_pt*.root /net/hisrv0001/home/"user"/tracking_eff_corrections/CMSSW_5_3_12_patch3/src/condor_trk_corr/final_hists/"}' | bash
 rm *.root
