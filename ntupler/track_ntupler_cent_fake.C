211b1b748abd7f0bb61cd13df2dc97f7ef0178a5
Austin A Baty <abaty@hidsk0001.cmsaf.mit.edu>
2014-11-21 15:53:57 -0500

    updated working fake stuff

--- ntupler/track_ntupler_cent_fake.C
+++ ntupler/track_ntupler_cent_fake.C
@@ -22,6 +22,9 @@
 #include "TNtuple.h"
 #include "TLine.h"
 #include "trackTree.C"
+#include "pfCand.C"
+#include "fragmentation_JEC_correction/fragmenation_JEC/fragmentation_JEC.h"
+
 
 void track_ntupler_cent_fake(int nstep_cent=2,int nstep_accept=1,int nstep_pt=1,int nstep_rmin=1,double bin_pt_min=8,double bin_pt_max=100,double bin_cent_min=0,double bin_cent_max=10,int *nevents=0){
  TH1D::SetDefaultSumw2();
@@ -40,6 +43,7 @@ void track_ntupler_cent_fake(int nstep_cent=2,int nstep_accept=1,int nstep_pt=1,
  infname[5] = "/HiForest_PYTHIA_HYDJET_pthat280_Track9_Jet30_matchEqR_merged_forest_0";
  infname[6] = "/HiForest_PYTHIA_HYDJET_pthat370_Track9_Jet30_matchEqR_merged_forest_0";
 
+ pfCand * fpf[7];
  trackTree * ftrk[7];
  HiTree * fhi[7];
  t * fjet[7];
@@ -50,6 +54,7 @@ void track_ntupler_cent_fake(int nstep_cent=2,int nstep_accept=1,int nstep_pt=1,
    ftrk[ifile] = new trackTree(Form("%s/%s.root",directory.Data(),infname[ifile]));
    fhi[ifile] = new HiTree(Form("%s/%s.root",directory.Data(),infname[ifile]));
    fjet[ifile] = new t(Form("%s/%s.root",directory.Data(),infname[ifile]));
+   fpf[ifile] = new pfCand(Form("%s/%s.root",directory.Data(),infname[ifile]));
    evtSelFile[ifile] = new TFile(Form("%s/%s.root",directory.Data(),infname[ifile]),"read");
    evtSel[ifile] = (TTree*) evtSelFile[ifile]->Get("skimanalysis/HltTree");
    evtSel[ifile]->SetBranchAddress("pcollisionEventSelection", &pcoll[ifile]); 
@@ -87,6 +92,10 @@ void track_ntupler_cent_fake(int nstep_cent=2,int nstep_accept=1,int nstep_pt=1,
  TH1F * centWeights = new TH1F("centWeight","centWeight",100,0,200);
  centWeights = (TH1F*)centWeightsFile->Get("centrality_weight");
 
+//initializing JEC Correction
+ fragmentation_JEC *FF_JEC=new fragmentation_JEC(3, true);
+ FF_JEC->set_correction();
+
  TFile *outf= new TFile(Form("track_ntuple_cent_%d_accept_%d_pt_%d_rmin_%d_ptmin%d_ptmax%d_centmin%d_centmax%d.root",nstep_cent,nstep_accept,nstep_pt,nstep_rmin,(int)bin_pt_min,(int)bin_pt_max,(int)bin_cent_min,(int)bin_cent_max),"recreate");
 
  std::string trackVars="pthat:pt:eta:phi:trkfake:trackselect:cent:rmin_reco:fake_cent:fake_accept:fake_pt:fake_rmin:fake:weight:pthat_weight:cent_weight";
@@ -103,6 +112,7 @@ void track_ntupler_cent_fake(int nstep_cent=2,int nstep_accept=1,int nstep_pt=1,
   ftrk[ifile]->GetEntry(jentry);
   fhi[ifile]->GetEntry(jentry);
   fjet[ifile]->GetEntry(jentry);
+  fpf[ifile]->GetEntry(jentry);
   evtSel[ifile]->GetEntry(jentry);
 
 //vertexShift can be used to move the center of the vertex distribution if needed
@@ -131,6 +141,37 @@ void track_ntupler_cent_fake(int nstep_cent=2,int nstep_accept=1,int nstep_pt=1,
 
   weight = pthat_weight*cent_weight;
  
+//implementing new FFcorrection; assumes input jet tree is sorted by pt
+  float jetPtCorr[10] = {0};
+  float jetEta[10] = {0};
+  float jetPhi[10] = {0};
+
+  for(int ijet=0;(ijet<fjet[ifile]->nref) && ijet<10 && fjet[ifile]->jtpt[ijet]>40;ijet++){
+    int npf=0;
+    for(int ipf=0;ipf<fpf[ifile]->nPFpart;ipf++){
+      if(FF_JEC->passes_PF_selection(fpf[ifile]->pfVsPt[ipf], fpf[ifile]->pfEta[ipf], fpf[ifile]->pfPhi[ipf], fpf[ifile]->pfId[ipf], fjet[ifile]->jteta[ijet], fjet[ifile]->jtphi[ijet])) npf++;
+    }
+    jetPtCorr[ijet]=FF_JEC->get_corrected_pt(fjet[ifile]->jtpt[ijet], npf);
+    jetEta[ijet] = fjet[ifile]->jteta[ijet];
+    jetPhi[ijet] = fjet[ifile]->jtphi[ijet];
+  }
+  int leadIndx = 0;
+  int subleadIndx = 0;
+  for(int i=0; i<10; i++){
+    if(jetPtCorr[leadIndx] < jetPtCorr[i]){
+      subleadIndx = leadIndx;
+      leadIndx = i;
+    }
+    else if (jetPtCorr[subleadIndx] < jetPtCorr[i]){
+      subleadIndx = i;
+    }
+  }
+  //std::cout << jetPtCorr[leadIndx] << " " << jetEta[leadIndx] << " " << jetPtCorr[subleadIndx] << " " << jetEta[subleadIndx] << " " << acos(cos(jetPhi[leadIndx]- jetPhi[subleadIndx])) << std::endl;
+  
+  //dijet cut is here!!!
+  if(jetPtCorr[leadIndx]<120 || jetPtCorr[subleadIndx]<50 || fabs(jetEta[leadIndx])>2 || fabs(jetEta[subleadIndx])>2 || acos(cos(jetPhi[leadIndx]- jetPhi[subleadIndx])) < 5*3.141592/6.0) continue;
+      
+
   for(int itrk=0;itrk<ftrk[ifile]->nTrk;itrk++){ 
 
    float pt=ftrk[ifile]->trkPt[itrk];
@@ -159,10 +200,9 @@ void track_ntupler_cent_fake(int nstep_cent=2,int nstep_accept=1,int nstep_pt=1,
     fake_pt=fake_pt+p_eff_pt[ipt]->GetBinContent(p_eff_pt[ipt]->GetXaxis()->FindBin(pt));    
    } 
    
-   for(int ijet=0;ijet<fjet[ifile]->nref;ijet++){
-    if(fabs(fjet[ifile]->jteta[ijet])>2 || fjet[ifile]->jtpt[ijet]<50) continue;
-    float r_reco=sqrt(pow(eta-fjet[ifile]->jteta[ijet],2)+pow(acos(cos(phi-fjet[ifile]->jtphi[ijet])),2));
- 
+   for(int ijet=0;ijet<10;ijet++){
+    if(fabs(jetEta[ijet])>2 || jetPtCorr[ijet]<50) continue;
+    float r_reco=sqrt(pow(eta-jetEta[ijet],2)+pow(acos(cos(phi-jetPhi[ijet])),2));
     if(r_reco<rmin_reco)rmin_reco=r_reco;
    }
    
@@ -182,6 +222,9 @@ void track_ntupler_cent_fake(int nstep_cent=2,int nstep_accept=1,int nstep_pt=1,
   outf->Close();
 
 //some memory cleanup
+
+  delete FF_JEC;
+  
   for(int icent=0; icent<nstep_cent;icent++){
   	f_eff_cent[icent]->Close();	
 	}
@@ -197,6 +240,7 @@ void track_ntupler_cent_fake(int nstep_cent=2,int nstep_accept=1,int nstep_pt=1,
   for(int ifile=0; ifile<7; ifile++){		
     fjet[ifile]->Close();
     fhi[ifile]->Close();
+    fpf[ifile]->Close();
     ftrk[ifile]->Close();
     evtSelFile[ifile]->Close();
   }
